cd9e0d34372959c0429f54cc5059713f9e804084
Carlos A. <carlosvsk@gmail.com>
2024-08-30 11:58:29 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -67,7 +67,7 @@ stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
           ],
 
           arrStyles: [
-            { color: 'black', fontSize: 12, }
+            stls.color({ pass: { arrayValue: ["white"] }})
           ],
 
           children: [
